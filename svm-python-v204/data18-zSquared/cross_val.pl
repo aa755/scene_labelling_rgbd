dfe2703f7470a37946cf6b0cf16fc80d96cdf5b3
Abhishek Anand <aa755@cs.cornell.edu>
2011-05-08 16:36:31 -0400

    added version and diff recording in logs

---
+++ svm-python-v204/data18-zSquared/cross_val.pl
@@ -0,0 +1,26 @@
+@C = (0.1);
+for $c (@C)
+{
+  print "C= $c\n";
+  for (my $i = 1; $i<= 4; $i++)
+  {
+    print "\tRunning $i fold ...\n";
+    chdir("fold$i");
+    $pwd = `pwd`;
+    print "$pwd\n";
+
+	`git log | head > logs/log.w4.$c.e.01.sum1.warm.IPafter10`
+	`git diff >> logs/log.w4.$c.e.01.sum1.warm.IPafter10`
+    my $train = `../../svm_python_learn --m svmstruct_mrf -c $c  -e 0.0001  train$i models/model.w4.$c.e.01.sum1.warm.IPafter10>> logs/log.w4.$c.e.01.sum1.warm.IPafter10 & `;
+   `sleep 300`;
+    chdir("../");
+  # `sleep 1200`;
+  }
+# for ( $i = 1; $i<= 5; $i++) 
+#  {
+#    print "\tRunning $i fold ...\n";
+#    my $accuracy = `./svm_python_classify  $d/data/test$i $d/models/model.$c.$i $d/pred/pred.$c.$i > $d/pred/out.$c.$i`;
+
+ # }
+}
+
