8baa48b446feafe6fb0b98e94cdca25fc0dd8e67
Abhishek Anand <aa755@cs.cornell.edu>
2011-05-18 02:10:17 -0400

    corrected the mrf cross val script

--- svm-python-v204/cross_val.pl
+++ svm-python-v204/cross_val.pl
@@ -1,5 +1,6 @@
-@C = (0.1);
+@C = (2);
 $e= 0.01;
+$method='qbpo';
 for $c (@C)
 {
   print "C= $c\n";
@@ -9,10 +10,11 @@ for $c (@C)
     chdir("fold$i");
     $pwd = `pwd`;
     print "$pwd\n";
+	$suffix="w4.c$c.e$e.$method";
 
-	`git log | head > logs/log.w4.$c.e.01.sum1.warm.IPafter10`;
-	`git diff >> logs/log.w4.$c.e.01.sum1.warm.IPafter10`;
-    my $train = `../../svm_python_learn --m svmstruct_mrf -c $c  -e $e  train$i models/model.w4.c$c.e$e.warm>> logs/log.w4.c$c.e$e.warm & `;
+	`git log | head > logs/log.$suffix`;
+	`git diff >> logs/log.$suffix`;
+    my $train = `../../svm_python_learn --m svmstruct_mrf -c $c  -e $e  train$i models/model.$suffix>> logs/log.$suffix & `;
    `sleep 300`;
     chdir("../");
   # `sleep 1200`;
