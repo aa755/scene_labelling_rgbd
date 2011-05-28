60f2511a7d4d739408a1d99cafce130363a05c24
Hema Koppula <hema@hemaHPUbuntu.(none)>
2011-05-28 15:04:55 -0400

    changes to include normalization and binning

--- svm-python-v204/initialize.sh
+++ svm-python-v204/initialize.sh
@@ -1,16 +1,34 @@
-mv /home/aa755/allData/data_nodefeats.txt ./
-mv /home/aa755/allData/data_edgefeats.txt ./
-mv /home/aa755/allData/data_scene_mapping.txt ./
-perl format.pl data_nodefeats.txt data_edgefeats.txt labelmap.txt
+#mv /home/aa755/allData/data_nodefeats.txt ./
+#mv /home/aa755/allData/data_edgefeats.txt ./
+#mv /home/aa755/allData/data_scene_mapping.txt ./
+rm data_nodefeats.*.txt data_edgefeats.*.txt
+rm datas_*.txt
+perl filter.pl data_nodefeats.txt
+perl filter.pl data_edgefeats.txt
+nohup matlab -nodesktop -nosplash -r normalize
+nohup matlab -nodesktop -nosplash -r binfeats
+cat header_data_nodefeats.txt temp_data_nodefeats.n.txt > data_nodefeats.n.txt
+cat header_data_edgefeats.txt temp_data_edgefeats.n.txt > data_edgefeats.n.txt
+cat header_data_nodefeats.txt temp_data_nodefeats.b.txt > data_nodefeats.b.txt
+cat header_data_edgefeats.txt temp_data_edgefeats.b.txt > data_edgefeats.b.txt
+rm temp_data_* 
+rm header_data_*
+perl format.pl data_nodefeats.b.txt data_edgefeats.b.txt labelmap.txt 1 
 folderName=`pwd | cut -f 7 -d '/'`
 echo "folder is $folderName"
 
 for i in `seq 1 4` 
 do
 
-sed s/data6-printer-rectified/$folderName/ fold$i/train$i >temp
+rm fold$i/pred/*
+rm fold$i/logs/*
+rm fold$i/out/*
+rm fold$i/models/*
+rm fold$i/imodels/*
+
+sed s/data23-edge-binned-template/$folderName/ fold$i/train$i >temp
 mv temp fold$i/train$i
 
-sed s/data6-printer-rectified/$folderName/ fold$i/test$i >temp
+sed s/data23-edge-binned-template/$folderName/ fold$i/test$i >temp
 mv temp fold$i/test$i
 done
