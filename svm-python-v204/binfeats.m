95fbc046b4578328c031e61e4bb4baf9898b8d82
Abhishek Anand <aa755@cs.cornell.edu>
2011-06-16 21:54:07 -0400

    binner now outputs the stumps

--- svm-python-v204/binfeats.m
+++ svm-python-v204/binfeats.m
@@ -1,5 +1,7 @@
 dataN = load('temp_data_nodefeats.n.txt');
 dataNB = dataN(:,1:3)
+numFeats=size(dataN,2)-4+1;
+binStumps=zeros(10,numFeats);
 for f = 4:size(dataN,2)
   featv = sort(dataN(:,f));
   binsize = floor(length(featv)/10)
@@ -7,14 +9,18 @@ for f = 4:size(dataN,2)
      binv(b) = featv(b*binsize)
   end
   binv(10) = featv(length(featv))
+  binStumps(:,f-3)=binv;
   for b =1:10
     dataNB = [dataNB dataN(:,f)<binv(b)];
   end
 end
 dlmwrite('temp_data_nodefeats.b.txt',dataNB,'\t')
+dlmwrite('binStumpsN.txt',binStumps','\t')
 
 dataE = load('temp_data_edgefeats.n.txt');
 dataEB = dataE(:,1:5)
+numFeats=size(dataE,2)-6+1;
+binStumps=zeros(10,numFeats);
 for f = 6:size(dataE,2)
   featv = sort(dataE(:,f));
   binsize = floor(length(featv)/10)
@@ -22,9 +28,11 @@ for f = 6:size(dataE,2)
      binv(b) = featv(b*binsize)
   end
   binv(10) = featv(length(featv))
+  binStumps(:,f-3)=binv;
   for b =1:10
     dataEB = [dataEB dataE(:,f)<binv(b)];
   end
 end
 dlmwrite('temp_data_edgefeats.b.txt',dataEB,'\t')
+dlmwrite('binStumpsE.txt',binStumps','\t')
 quit
