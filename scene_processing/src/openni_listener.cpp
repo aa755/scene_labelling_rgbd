1fe4206d864131864ed48291ea642104440ee3d6
Abhishek Anand <aa755@cs.cornell.edu>
2011-06-20 00:37:07 -0400

    implemented simple subscriber

--- scene_processing/src/openni_listener.cpp
+++ scene_processing/src/openni_listener.cpp
@@ -1860,21 +1860,14 @@ int write_feats(TransformG transG,  pcl::PointCloud<pcl::PointXYZRGBCamSL>::Ptr 
 
 
 }
-
+/*
 OpenNIListener::OpenNIListener( ros::NodeHandle nh,  const char* visual_topic, 
                                const char* depth_topic, const char* info_topic, 
                                const char* cloud_topic, const char* filename , unsigned int step ) 
-: visual_sub_ (nh, visual_topic, 10),
-  depth_sub_(nh, depth_topic, 10),
-  info_sub_(nh, info_topic, 10),
-  cloud_sub_(nh, cloud_topic, 10),
-  sync_(MySyncPolicy(10),  visual_sub_, depth_sub_, info_sub_, cloud_sub_),
-  callback_counter_(0),
-  step_(step)
 {
   // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
-  sync_.registerCallback(boost::bind(&OpenNIListener::cameraCallback, this, _1, _2, _3, _4));
-  ROS_INFO_STREAM("OpenNIListener listening to " << visual_topic << ", " << depth_topic \
+ // cloud_sub_=nh.subscribe(cloud_topic,2,cameraCallback);
+//  ROS_INFO_STREAM("OpenNIListener listening to " << visual_topic << ", " << depth_topic \
                    << ", " << info_topic << " and " << cloud_topic << "\n"); 
 
  // bag_.open(filename, rosbag::bagmode::Write);
@@ -1888,30 +1881,32 @@ OpenNIListener::OpenNIListener( ros::NodeHandle nh,  const char* visual_topic,
            "/rgbdslam/input/image_mono", 10);
   pub_depth_ = nh.advertise<sensor_msgs::Image> (
            "/rgbdslam/input/depth_image", 10);
-*/
+//
 
 }
-
+*/
+        
 TransformG globalTransform;
 
-void OpenNIListener::cameraCallback (const sensor_msgs::ImageConstPtr& visual_img_msg, 
+void cameraCallback (/*const sensor_msgs::ImageConstPtr& visual_img_msg, 
                                      const sensor_msgs::ImageConstPtr& depth_img_msg,   
-                                     const sensor_msgs::CameraInfoConstPtr& cam_info,
+                                     const sensor_msgs::CameraInfoConstPtr& cam_info,*/
                                      const sensor_msgs::PointCloud2ConstPtr& point_cloud) {
 
-  
-   if(++callback_counter_%step_ == 0) {
+  static int callback_counter_=0;
+  callback_counter_++;
+ //  if(++callback_counter_%step_ == 0) {
    ROS_INFO("Received data from kinect");
-   VectorG origin(0,0,0);
+   
        pcl::PointCloud<pcl::PointXYZRGB> cloud;
        pcl::PointCloud<pcl::PointXYZRGBCamSL>::Ptr cloud_seg_ptr(new pcl::PointCloud<pcl::PointXYZRGBCamSL > ());
        pcl::fromROSMsg(*point_cloud, cloud);
-       convertType(cloud,*cloud_seg_ptr,origin,0);
+       convertType(cloud,*cloud_seg_ptr,globalTransform.getOrigin(),0);
        assert(cloud_seg_ptr->size()==640*480);
        segmentInPlace(*cloud_seg_ptr);
        write_feats(globalTransform,cloud_seg_ptr,callback_counter_);
        
-   }
+   //}
 }
 
 int main(int argc, char** argv)
@@ -1928,13 +1923,8 @@ int main(int argc, char** argv)
   
   readInvLabelMap(invLabelMap,"../svm-python-v204/home_labelmap.txt");
   globalTransform=readTranform("globalTransform.bag");
-  OpenNIListener kinect_listener(n, 
-                                 "/camera/rgb/image_mono",  
-                                 "/camera/depth/image",
-								 "/camera/rgb/camera_info",
-								 "/camera/rgb/points");
-  
- 
+  ros::Subscriber cloud_sub_=n.subscribe("/rgbdslam/my_clouds",2,cameraCallback);
+								 
    ros::spin();
   
 
