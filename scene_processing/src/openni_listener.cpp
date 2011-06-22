764a4394418d8f3a536f00164be32963117e63e3
nav <nav@nav-stair.(none)>
2011-06-22 13:53:42 -0400

    removed lapack, made changes to manifest to get proper includes

--- scene_processing/src/openni_listener.cpp
+++ scene_processing/src/openni_listener.cpp
@@ -28,8 +28,8 @@ typedef pcl::PointXYZRGBCamSL PointT;
 //#include<boost/numeric/bindings/traits/ublas_matrix.hpp>
 //#include<boost/numeric/bindings/lapack/gels.hpp>
 //#include <boost/numeric/bindings/traits/ublas_vector2.hpp>
-namespace ublas = boost::numeric::ublas;
-namespace lapack= boost::numeric::bindings::lapack;
+//namespace ublas = boost::numeric::ublas;
+//namespace lapack= boost::numeric::bindings::lapack;
 #include "pcl_visualization/pcl_visualizer.h"
 typedef pcl_visualization::PointCloudColorHandler<sensor_msgs::PointCloud2> ColorHandler;
 typedef ColorHandler::Ptr ColorHandlerPtr;
