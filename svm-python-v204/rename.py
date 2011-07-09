eaefeb95a34277a6eb5dba3ce2393c3d67387cc5
aa755 <aa755@cs.cornell.edu>
2011-07-08 20:57:24 -0400

    final version of automated rename script

--- svm-python-v204/rename.py
+++ svm-python-v204/rename.py
@@ -6,9 +6,10 @@ __author__="aa755"
 __date__ ="$Jul 8, 2011 6:54:07 PM$"
 
 import commands;
+from scipy.constants.constants import fine_structure
 import re;
 
-out=commands.getoutput('ls *.png');
+out=commands.getoutput('ls *.pcd');
 files=out.split();
 
 scene_map={};
@@ -25,10 +26,14 @@ for line in mapfile:
 #rename the files now
 for file in files:
 #    matches=re.search('([0-9]*)_(transformed_labeled_data_.*_segmented_xyzn\.pcd\.bag)\.pcd\.png',file)
-    matches=re.search('([0-9]*)_(transformed_labeled_data.*_segmented_xyzn.*)',file)
+    matches=re.search('([0-9]*)_(transformed_labeled_data.*_segmented_xyzn(_binary)?\.pcd)',file)
     frameNum=matches.group(1);
     imgfilename=matches.group(2);
     imflen=len(imgfilename);
-    bagfilename=imgfilename[0:imflen-12];
-    newName='scene'+scene_map[bagfilename]+'_'+frameNum
-    print file, "->",newName
+    bagfilename=imgfilename#[0:imflen-12];
+    newName='scene'+scene_map[bagfilename]+'_'+frameNum + file[len(file)-4:len(file)];
+    #print file, "->",newName
+    stat_out=commands.getstatusoutput( 'mv ' +file +' '+ newName);
+    if(stat_out[0]!=0):
+        print 'error: ',stat_out[1];
+        exit(-1);
