5a702e0f609572aca2bbcd8ab60b2d0c71bc4a73
Abhishek Anand <abhishek@abhishek-tablet.(none)>
2011-07-08 21:24:50 -0700

    deleted unnecessary files

--- poc/run.m
+++
@@ -1,9 +0,0 @@
-function [a , b, theta] = run(X , Y, K,L)
-
-theta = inv(X'*X) * X'*Y;
-y_pred = X*theta;
-y_pred_vec = y_pred';
-y_pred_vec = y_pred_vec(:);
-[a,b]  = my_eval(y_pred_vec, Y,K,L);
-
-end
--- poc/run_test.m
+++
@@ -1,8 +0,0 @@
-function [a , b] = run_test(X , Y, theta, K,L)
-
-y_pred = X*theta;
-y_pred_vec = y_pred';
-y_pred_vec = y_pred_vec(:);
-[a,b]  = my_eval(y_pred_vec, Y,K,L);
-
-end
