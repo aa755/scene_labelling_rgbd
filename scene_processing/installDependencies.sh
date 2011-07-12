7582fa210b25ef5f5936f059747d264c361f05fd
abhishek <abhishek@abhishek-HP-tablet.(none)>
2011-07-11 21:22:38 -0400

    improved scripts

--- scene_processing/installDependencies.sh
+++ scene_processing/installDependencies.sh
@@ -1,4 +1,4 @@
-echo "installing some dependencies .... if all steps succed, you will see Success!!  else see comments in this script for possible fixes"
+echo "installing some dependencies .... if all steps succed, you will see Success!!  else see comments in this script for possible fixes. Yo must have ROS installed(we tested on diamondback)... if not, please visit: http://www.ros.org/wiki/diamondback/Installation/Ubuntu"
 set -e
 cd boost-numeric-bindings/
 echo "installing boost numeric bindings for gels least squares fitting and others"
@@ -6,7 +6,7 @@ echo "installing boost numeric bindings for gels least squares fitting and other
 make
 sudo make install
 cd ..
-sudo apt-get install subversion ros-diamondback-octomap-mapping
+sudo apt-get install subversion ros-diamondback-octomap-mapping ros-diamondback-perception-pcl ros-diamondback-perception-pcl-addons
 cd ..
 svn co https://code.ros.org/svn/wg-ros-pkg/branches/trunk_boxturtle/stacks/semantic_mapping
 rosstack find semantic_mapping # scene_labelling must be in ROS_PACKAGE_PATH
